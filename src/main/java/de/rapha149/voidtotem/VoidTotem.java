f3a4a8f385f9ee18a72dce4d3e4d24b9cacdcd90
Rapha149 <raphael@raphael-jantzen.de>
2022-01-02 13:29:52 +0100

    Initial commit

---
+++ src/main/java/de/rapha149/voidtotem/VoidTotem.java
@@ -0,0 +1,17 @@
+package de.rapha149.voidtotem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VoidTotem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
